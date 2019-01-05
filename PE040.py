c8de576c1c16c091c3fe94126b8549566c621e80
jeanremy-berard <46303388+jeanremy-berard@users.noreply.github.com>
2019-01-05 17:38:06 +0100

    Create PE040.py

---
+++ PE040.py
@@ -0,0 +1,5 @@
+str_dec = ''
+for i in range(1000001):
+    str_dec += str(i)
+
+int(str_dec[1]) * int(str_dec[10]) * int(str_dec[100]) * int(str_dec[1000]) * int(str_dec[10000]) * int(str_dec[100000]) * int(str_dec[1000000])
